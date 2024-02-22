eef248e5dc1eaf502f0d17560e8ed4de5a48de03
sonatejasd <sonatejasd@gmail.com>
2024-02-22 15:47:59 +0530

    added styles and JS logic

--- session35.js
+++ session35.js
@@ -1,1 +1,35 @@
-let c = document.getElementById("counter")
+let c = document.getElementById("count");
+let eo = document.getElementById("eo");
+let count = 0;
+function incr() {
+    if(count<20){
+        count++;
+        display();
+    }
+    else{
+        alert("maximum count exceeded");
+    }
+}
+function decr() {
+    if(count!=0){
+        count--;
+        display();
+    }
+    else{
+        alert("minimum count is 0");
+    }
+}
+function display() {
+    //alert("you clicked the button!");
+    c.textContent=count;
+    if(count%2 == 0) {
+        eo.textContent = 'even'
+    }else{
+        eo.textContent = 'odd';
+    }
+}
+
+function reset() {
+    count = 0;
+    display()
+}
